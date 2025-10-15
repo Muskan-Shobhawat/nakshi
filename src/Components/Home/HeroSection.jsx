09b4354ca0508871563faca67f7c5837abe0831d
Muskan Shobhawat <muskanshobhawat123@gmail.com>
2025-10-15 18:32:12 +0530

    herosectionfix

--- src/Components/Home/HeroSection.jsx
+++ src/Components/Home/HeroSection.jsx
@@ -1,21 +1,24 @@
-import React from 'react';
-import heroImage from '../../assets/heroimg2.png'; // Replace with jewelry image
+import React from "react";
+import heroImage from "../../assets/heroimg2.jpg"; // Replace with jewelry image
 import "../../CSS/Home/HeroSection.css";
 
 function HeroSection() {
   return (
     <section
-      className="hero-section text-light d-flex align-items-center hrfix"
+      className="hero-section text-light hrfix"
       style={{
         backgroundImage: `url(${heroImage})`,
         backgroundSize: 'cover',
-        backgroundPosition: '0vw -5vh',
-        height: '90vh'
+        backgroundPosition: '0vw 0vh',
+        backgroundRepeat:'no-repeat'
+      //   height: '90vh'
       }}
     >
-      <div className="container text-center ff">
+      <div className=" text-left ff">
         <h1 className="display-4 fw-bold">Golden Touch to Your Everyday</h1>
-        <p className="lead">Premium 1gm Gold-Plated Designs for Timeless Beauty</p>
+        <p className="lead">
+          Premium 1gm Gold-Plated Designs for Timeless Beauty
+        </p>
         <a href="#" className="btn btn-outline-light btn-lg mt-3 exp">
           Explore Now
         </a>
