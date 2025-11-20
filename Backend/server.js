a958fd4fc3f016785e8e13179d37b4b827b6f77e
Muskan Shobhawat <muskanshobhawat123@gmail.com>
2025-11-20 13:47:28 +0530

    Cart

--- Backend/server.js
+++ Backend/server.js
@@ -69,7 +69,7 @@ console.log("Mongo URI:", process.env.MONGO_URI);
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(PORT, '0.0.0.0', () => {
+    app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
   })
